5a3199a7b370e4f4676e26651fdf7d45321f3ed6
jnboakye <jaeyblaq2131@gmail.com>
2025-03-17 13:31:26 +0000

    secomd commit

--- models/wishlist.js
+++ models/wishlist.js
@@ -4,9 +4,8 @@ import { Schema, model } from "mongoose";
 const wishlistSchema = new Schema({
     name: { type: String, required: true },
     price: { type: String, required: true },
-    storeLink: { type: String, required: true }, 
-    { timestamps: true }
-}
-);
+    storeLink: { type: String, required: true }
+}, { timestamps: true })
+
 
 export const WishlistModel = model('Wishlist', wishlistSchema);
